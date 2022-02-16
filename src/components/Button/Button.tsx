b8db38b4f84f4188eae2cd48f1bcf6d2d60567b4
krishna <devenger@192.168.1.5>
2022-02-16 22:59:28 +0530

    Added styles for buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,31 @@ import "./Button.css";
 
 export interface ButtonProps {
   label: string;
+  type: any;
 }
 
+const primaryStyle = {
+  color: "#fff",
+  backgroundColor: "#0d6efd",
+  borderColor: "#0d6efd",
+};
+
+const successStyle = {
+  color: "#fff",
+  backgroundColor: "#198754",
+  borderColor: "#198754",
+};
+
+const setClass = (type: any) => {
+  if (type === "success") {
+    return successStyle;
+  }
+
+  return primaryStyle;
+};
+
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button style={setClass(props.type)}>{props.label}</button>;
 };
 
 export default Button;
